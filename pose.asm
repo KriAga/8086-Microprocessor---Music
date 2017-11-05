9b5ec437f95dec4563a7fe79c42d2e4e7282a810
Krishna Agarwal <kriaga3@gmail.com>
2017-11-06 02:34:40 +0530

    beep and pause files added

---
+++ pose.asm
@@ -0,0 +1,82 @@
+data segment
+data ends
+
+code segment
+        assume ds:data,cs:code;cs:data
+start:
+        
+        mov     al, 182        
+        out     43h, al        
+        mov     ax, 9121        
+                                
+        out     42h, al       
+        mov     al, ah         
+        out     42h, al 
+        in      al, 61h       
+                               
+        or      al, 00000011b  
+        out     61h, al        
+        mov     bx, 15         
+pause11:
+        mov     cx, 65535
+pause12:
+        dec     cx
+        jne     pause12
+        dec     bx
+        jne     pause11
+
+
+        mov     al, 182       
+        out     43h, al      
+        mov     ax, 0001      
+                               
+        out     42h, al        
+        mov     al, ah          
+        out     42h, al 
+        in      al, 61h        
+                               
+        or      al, 00000011b  
+        out     61h, al        
+        mov     bx, 10         
+pause13:
+        mov     cx, 65535
+pause14:
+        dec     cx
+        jne     pause14
+        dec     bx
+        jne     pause13
+
+
+
+        mov     al, 182        
+        out     43h, al        
+        mov     ax, 9121     
+                               
+        out     42h, al       
+        mov     al, ah         
+        out     42h, al 
+        in      al, 61h        
+                               
+        or      al, 00000011b 
+        out     61h, al        
+        mov     bx, 15      
+pause1:
+        mov     cx, 65535
+pause2:
+        dec     cx
+        jne     pause2
+        dec     bx
+        jne     pause1
+
+
+        
+        in      al, 61h         
+                                
+        and     al, 11111100b   
+        out     61h, al         
+
+        mov ah,4ch
+        int 21h
+        hlt
+code ends
+end start
